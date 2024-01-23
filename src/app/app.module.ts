6bb767a778407f6d96167f56c6e55a3d97793a05
Gladys Waireri <Gmumbi660@gmail.com>
2024-01-23 15:18:44 +0300

    Additions to the original commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; //Where NgModel is located
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomesComponent } from './homes/homes.component';
+import { HomesComponent } from './homes/homes.component'; //declaration of the HomesComponent removed because it is "unused"
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomesComponent,
-  ],
+   HomesComponent,//deleted HomesComponent since it cannot be part of the NgModule
+   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    FormsModule, //added when we imported the FormsModule symbol
     AppRoutingModule
   ],
   providers: [],
--- src/app/homes/homes.component.ts
+++ src/app/homes/homes.component.ts
@@ -1,21 +1,13 @@
 import { Component } from '@angular/core';
 import { HOMES } from '../mock-homes';
-import{
-/*...*/
-NgFor,
-/*...*/
-} from '@angular/common';
+
 
 @Component({
   selector: 'app-homes',
   templateUrl: './homes.component.html',
   styleUrl: './homes.component.css',
-  standalone:true,
-  imports:[
-    NgFor,
-  ],
-})
-
+}
+)
 export class HomesComponent {
 homes = HOMES;
 }
